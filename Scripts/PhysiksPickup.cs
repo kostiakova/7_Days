0a247ecd5878b771379dbd53029bdd7555468953
vanya12312e <112240505+vanya12312e@users.noreply.github.com>
2023-05-14 14:34:20 +0300

    Add files via upload

--- Scripts/PhysiksPickup.cs
+++ Scripts/PhysiksPickup.cs
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 using UnityEngine;
 [RequireComponent(typeof(Rigidbody))]
 
@@ -46,47 +45,3 @@ public class PhysiksPickup : MonoBehaviour
     }
       
 }
-=======
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class PhysiksPickup : MonoBehaviour
-{
-    [SerializeField]private LayerMask _pickupMask;
-    [SerializeField]private Camera _playerCamera;
-    [SerializeField]private Transform _pickupPos;
-    [Space]
-    [SerializeField]private float _pickupRange;
-    [SerializeField]private Rigidbody _currentObject;
-
-    private void Update()
-    {
-        if(Input.GetKeyDown(KeyCode.E))
-        {
-            if(_currentObject)
-            {
-                _currentObject.useGravity = true;
-                _currentObject = null;
-                return;
-            }
-            Ray CameraRay = _playerCamera.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0.5f));
-            if (Physics.Raycast(CameraRay, out RaycastHit hitInfo, _pickupRange, _pickupMask))
-            {
-                _currentObject = hitInfo.rigidbody;
-                _currentObject.useGravity = false;
-            }
-        }
-    }
-
-    private void FixedUpdate()
-    {
-        if(_currentObject)
-        {
-            Vector3 DirectionToPoint = _pickupPos.position - _currentObject.position;
-            float DistanceToPoint = DirectionToPoint.magnitude;
-            _currentObject.velocity = DirectionToPoint * 12 * DistanceToPoint;
-        }
-    }
-}
->>>>>>> c12922ad3d961c3f2fd4fe41c8bd997e9cc95982
