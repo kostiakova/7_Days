e3e1930947dc006303dcb8b471684695ddf25654
vanya12312e <112240505+vanya12312e@users.noreply.github.com>
2023-05-14 14:32:19 +0300

    Pickup

--- Scripts/PhysiksPickup.cs
+++ Scripts/PhysiksPickup.cs
@@ -1,3 +1,52 @@
+<<<<<<< HEAD
+using UnityEngine;
+[RequireComponent(typeof(Rigidbody))]
+
+public class PhysiksPickup : MonoBehaviour
+{
+    [SerializeField]private LayerMask _pickupMask;
+    [SerializeField]private Camera _playerCamera;
+    [SerializeField]private float _pickupRange;
+
+    [SerializeField]private Rigidbody _currnetObjectRigidbody;
+    private Collider _currntObjectCollider;
+    public Transform _hand;
+
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.E))
+        {
+            Ray PickupRay = new Ray(_playerCamera.transform.position, _playerCamera.transform.forward);
+            if (Physics.Raycast(PickupRay, out RaycastHit hitInfo, _pickupRange, _pickupMask))
+            {
+                _currnetObjectRigidbody.position = _hand.position;
+                _currnetObjectRigidbody.rotation = _hand.rotation;
+
+                if (_currnetObjectRigidbody)
+                {
+
+                }
+                else
+                {
+                    _currnetObjectRigidbody = hitInfo.rigidbody;
+                    _currntObjectCollider = hitInfo.collider;
+                    _currnetObjectRigidbody.isKinematic = true;
+                    _currntObjectCollider.enabled = false;
+
+                }
+            }
+            if(_currnetObjectRigidbody)
+            {
+                
+            }
+
+        }
+
+    }
+      
+}
+=======
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -40,3 +89,4 @@ public class PhysiksPickup : MonoBehaviour
         }
     }
 }
+>>>>>>> c12922ad3d961c3f2fd4fe41c8bd997e9cc95982
